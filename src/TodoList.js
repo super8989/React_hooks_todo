1578e7953313e544e4339ff0b5a8acb144514f3a
Sam <super8989@gmail.com>
2019-12-28 22:09:12 -0500

    imported material-ui and rendered list items using material-ui

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,25 @@
 import React from "react";
 
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
 function TodoList(props) {
 	return (
-		<div>
-			{props.todos.map(todo => (
-				<li>{todo.task}</li>
-			))}
-		</div>
+		<Paper>
+			<List>
+				{props.todos.map(todo => (
+					<>
+						<ListItem>
+							<ListItemText>{todo.task}</ListItemText>
+						</ListItem>
+						<Divider />
+					</>
+				))}
+			</List>
+		</Paper>
 	);
 }
 
