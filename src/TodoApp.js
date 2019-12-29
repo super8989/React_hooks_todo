871f09cc752d3d5a49be412a7a6ae267e973d668
Sam <super8989@gmail.com>
2019-12-28 21:57:11 -0500

    added AppBar

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
 
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+
 function TodoApp() {
-	return <h1>Todos</h1>;
+	return (
+		<Paper
+			style={{
+				padding: 0,
+				margin: 0,
+				height: "100vh",
+				backgroundColor: "#fafafa"
+			}}
+			elevation={0}
+		>
+			<AppBar color='primary' position='static' style={{ height: "64px" }}>
+				<Toolbar>
+					<Typography color='inherit'>Todos with Hooks</Typography>
+				</Toolbar>
+			</AppBar>
+		</Paper>
+	);
 }
 
 export default TodoApp;
