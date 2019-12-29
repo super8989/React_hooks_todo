826dd84f539bef444ce637eee6ef6c125aeb869e
Sam <super8989@gmail.com>
2019-12-28 21:51:47 -0500

    skeleton for TodoApp using hooks

---
+++ src/TodoApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+function TodoApp() {
+	return <h1>Todos</h1>;
+}
+
+export default TodoApp;
+
+// - TodoApp
+//   - TodoForm
+//   - TodoList
+//     - TodoItem
