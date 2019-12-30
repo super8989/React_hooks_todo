a26de5ff4ea91b8b90cab19c4c8f8dc852963b85
Sam <super8989@gmail.com>
2019-12-29 22:39:51 -0500

    implemented useInputState hook in TodoForm.js

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -3,10 +3,15 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 
+import useInputState from "./hooks/useInputState";
+
 function TodoForm(props) {
+	const [value, handleChange, reset] = useInputState("");
+
 	return (
 		<Paper>
-			<TextField />
+			{value}
+			<TextField value={value} onChange={handleChange} />
 		</Paper>
 	);
 }
