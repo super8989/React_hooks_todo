fd65bf067ae9b9ff7bdb65fc295ce45819cdfee6
Sam <super8989@gmail.com>
2019-12-29 22:36:19 -0500

    created useInputState hook in hooks directory

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -3,7 +3,7 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 
-function TodoForm() {
+function TodoForm(props) {
 	return (
 		<Paper>
 			<TextField />
