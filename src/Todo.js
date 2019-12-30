3de2fd4bebbc8eba13578b216e09bb1294d76483
Sam <super8989@gmail.com>
2019-12-29 23:15:19 -0500

    added a Checkbox to Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
 
 function Todo({ task, completed }) {
 	return (
 		<ListItem>
+			<Checkbox tabIndex={-1} checked={completed} />
 			<ListItemText>{task}</ListItemText>
 		</ListItem>
 	);
