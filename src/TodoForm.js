16e6a717bab8573198e03c1d5f4dc40728a8c04e
Sam <super8989@gmail.com>
2019-12-29 22:48:02 -0500

    added a form to submit a todo in TodoForm.js which adds the todo to state in TodoApp.js

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -5,13 +5,20 @@ import Paper from "@material-ui/core/Paper";
 
 import useInputState from "./hooks/useInputState";
 
-function TodoForm(props) {
+function TodoForm({ addTodo }) {
 	const [value, handleChange, reset] = useInputState("");
 
 	return (
 		<Paper>
-			{value}
-			<TextField value={value} onChange={handleChange} />
+			<form
+				onSubmit={e => {
+					e.preventDefault();
+					addTodo(value);
+					reset();
+				}}
+			>
+				<TextField value={value} onChange={handleChange} />
+			</form>
 		</Paper>
 	);
 }
