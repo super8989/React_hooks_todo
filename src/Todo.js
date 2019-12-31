93d63c336837328bff94ec249c8ba1370c37585f
Sam <super8989@gmail.com>
2019-12-30 20:09:23 -0500

    added in an EditIcon to Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -5,6 +5,7 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 
 function Todo({ task, completed }) {
@@ -15,6 +16,9 @@ function Todo({ task, completed }) {
 			<ListItemSecondaryAction>
 				<IconButton>
 					<DeleteIcon />
+				</IconButton>
+				<IconButton>
+					<EditIcon />
 				</IconButton>
 			</ListItemSecondaryAction>
 		</ListItem>
