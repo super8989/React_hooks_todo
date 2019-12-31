f27b8a6e5b421b382de68a49aaa25769a167a744
Sam <super8989@gmail.com>
2019-12-30 22:47:03 -0500

    removed the divider from the last todo item

--- src/TodoList.js
+++ src/TodoList.js
@@ -10,7 +10,7 @@ function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
 	return (
 		<Paper>
 			<List>
-				{todos.map(todo => (
+				{todos.map((todo, i) => (
 					<>
 						<Todo
 							id={todo.id}
@@ -21,7 +21,7 @@ function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
 							toggleTodo={toggleTodo}
 							editTodo={editTodo}
 						/>
-						<Divider />
+						{i < todos.length - 1 && <Divider />}
 					</>
 				))}
 			</List>
